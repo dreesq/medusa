07e718c9b5442c8180d134be1870649223e66fd2
bthe0 <theo@btheo.com>
2019-02-04 01:57:02 +0200

    Latest changes.

--- client/src/components/Auth/index.js
+++ client/src/components/Auth/index.js
@@ -1,11 +1,38 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import {parse} from "../../utils";
+import client from '../../client';
 
 export default class Auth extends Component {
+    state = {
+        form: {},
+        error: {}
+    };
+
+    login = async e => {
+        const parsed = parse(e);
+
+        const a = await client.login({
+            ...parsed,
+            provider: 'local'
+        });
+
+        console.log(a);
+    };
+
     render() {
         return (
             <div>
                 <h4>Sign In</h4>
+                <p>Don't have an account? <Link to={'/auth/register'}>Register</Link></p>
+
+                <form onSubmit={this.login}>
+                    <label>Email</label>
+                    <input type={'email'} placeholder={'Email'} name={'email'} />
+                    <label>Password</label>
+                    <input type={'password'} name={'password'} />
+                    <button>Login</button>
+                </form>
             </div>
         );
     }
