42e55197965f98cffe5ad5c6f0956fa14fd486d5
bthe0 <theo@btheo.com>
2019-02-09 02:02:05 +0200

    Register user page.

--- client/src/components/Auth/Register.js
+++ client/src/components/Auth/Register.js
@@ -1,12 +1,86 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import {Button, Card, Col, Container, Form, FormGroup, Input, Label, Row} from 'reactstrap';
+import Alert from '../Misc/Alert';
+import {withRouter} from 'react-router-dom';
+import {parse} from '../../utils';
+import client from '../../client';
 
-export default class Register extends Component {
+class Register extends Component {
+    state = {
+        loading: false
+    };
+
+    register = async e => {
+        const parsed = parse(e);
+        this.loading(true);
+
+        const {data, errors} = await client.createUser({
+            ...parsed,
+            provider: 'local'
+        });
+
+        this.loading(false);
+
+        if (errors) {
+            return this.alert.toggle(errors);
+        }
+
+        const {data: user, errors: authErrors} = await client._auth.login({
+            ...parsed,
+            provider: 'local'
+        });
+
+        if (authErrors) {
+            return this.alert.toggle(authErrors);
+        }
+
+        const {history} = this.props;
+        history.push('/');
+    };
+
+    loading = (loading = true) => {
+        this.setState({ loading });
+    };
+
     render() {
+        const {loading} = this.state;
+
         return (
-            <div>
-                <h4>Sign Up</h4>
-            </div>
+            <Container>
+                <Row>
+                    <Col lg={{size: 4, offset: 4}}>
+                        <Card className={'p-4 mt-5'}>
+                            <div className={'text-center'}>
+                                <h4>Sign Up</h4>
+                                <p>Already have an account? <Link to={'/auth'}><u>Sign in</u></Link> instead</p>
+                            </div>
+                            <Alert ref={ref => this.alert = ref}/>
+                            <Col lg={12}>
+                                <Form onSubmit={this.register}>
+                                    <FormGroup>
+                                        <Label>Name</Label>
+                                        <Input type={'text'} name={'name'} autoFocus={true} placeholder={'Name'}/>
+                                    </FormGroup>
+                                    <FormGroup>
+                                        <Label>Email</Label>
+                                        <Input type={'email'} name={'email'} placeholder={'me@email.com'}/>
+                                    </FormGroup>
+                                    <FormGroup>
+                                        <Label>Password</Label>
+                                        <Input type={'password'} name={'password'} placeholder={'Password'}/>
+                                    </FormGroup>
+                                    <Button block color={'primary'} disabled={loading}>
+                                        Submit
+                                    </Button>
+                                </Form>
+                            </Col>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
         );
     }
 }
+
+export default withRouter(Register);
